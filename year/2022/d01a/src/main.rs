cd1aadcdca211dbc8340efef54bf742351782626
Traydr <Traydr@protonmail.com>
2022-12-04 14:58:36 +0200

    d01 part 1

--- year/2022/d01a/src/main.rs
+++ year/2022/d01a/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+    let mut max_calories: u32 = 0;
+
+    let mut temp_calories: u32 = 0;
+    for var in input.lines() {
+        if str::is_empty(var) {
+            if temp_calories > max_calories { max_calories = temp_calories }
+            temp_calories = 0;
+            continue;
+        }
+
+        temp_calories += var.parse().unwrap_or(0);
+    }
+    println!("{}", max_calories)
 }
