93609a3421d9626b2f06d695b7d815938f125a1e
Traydr <Traydr@protonmail.com>
2022-12-04 17:26:01 +0200

    day3 part1

---
+++ year/2022/d03a/src/main.rs
@@ -0,0 +1,33 @@
+const LOWERCASE_OFFSET: i32 = 96;
+const UPPERCASE_OFFSET: i32 = 64 - 26;
+
+fn main() {
+    let input = include_str!("../input.txt");
+
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let length = line.len();
+        let halves = line.clone().split_at(length/2);
+
+        println!("Original:\n\t{}\nSplit:\n\t{} {}", line, halves.0, halves.1);
+
+        for c in halves.0.chars() {
+            if halves.1.contains(c) {
+                println!("Char {} is int {}", c, char_to_int(c));
+                sum += char_to_int(c);
+                break;
+            }
+        }
+    }
+
+    println!("{}", sum);
+}
+
+fn char_to_int(c: char) -> i32 {
+    if c.is_lowercase() {
+        return (c as i32) - LOWERCASE_OFFSET;
+    } else {
+        return (c as i32) - UPPERCASE_OFFSET;
+    }
+}
