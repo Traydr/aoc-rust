90b70847ddacd36497bc23704c9eef9d9583d0a4
Traydr <Traydr@protonmail.com>
2022-12-09 12:33:22 +0200

    To be redone day5 part1

--- year/2022/d05a/src/main.rs
+++ year/2022/d05a/src/main.rs
@@ -1,9 +1,29 @@
 fn main() {
     let input = include_str!("../input.txt");
+    let mut stack_input: Vec<&str> = Vec::new();
+    let mut stacks: Vec<Vec<char>>;
+    let mut num_stacks = 0;
 
     for line in input.lines() {
+        if str::is_empty(line) { continue; }
 
+        if line.chars().nth(0).unwrap() == '[' {
+            println!("Pushing line to stack: {}", line);
+            stack_input.push(line);
+            continue;
+        } else if line.chars().nth(1).unwrap() == '1' {
+            println!(" Num Line: {}", line);
+            num_stacks = line.split(" ").into_iter().last().unwrap().parse::<i32>().unwrap();
+
+            stacks = Vec::with_capacity(num_stacks as usize);
+            for _i in 1..num_stacks {
+                stacks.push(Vec::new());
+            }
+
+            stack_input.reverse();
+            for i in 0..num_stacks {
+                let stack_input_line = stack_input.pop().unwrap();
+            }
+        }
     }
-
-    println!("Hello, world!");
 }
